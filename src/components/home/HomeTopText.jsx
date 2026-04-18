1ba111083c2fd9f13681b0c6caf22510b45f9e8a
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-18 23:16:42 +0530

    added a video tag with some custom values

--- src/components/home/HomeTopText.jsx
+++ src/components/home/HomeTopText.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default function HomeTopText() {
-  return (
+  return ( 
     <>
-    <div  className='font font-[font1]'>
-    <div className='text-[12vw] uppercase  text-amber-50'>The spark for</div>
-    <div className='font text-[12vw] uppercase  text-amber-50'>allthings</div>
-    <div className='font text-[12vw] uppercase  text-amber-50'>creative</div>
+    <div  className='font font-[font1] pt-5 text-center'>
+    <div className='text-[8vw] uppercase      leading-[10vw] text-amber-50'>The spark for</div>
+    <div className='font text-[8vw] uppercase leading-[10vw] text-amber-50'>allthings</div>
+    <div className='font text-[8vw] uppercase leading-[10vw] text-amber-50'>creative</div>
     </div>
     </>
   )
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Video() {
   return (
     <div className='h-screen '>
-            <video   className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1776482898~exp=1776486498~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=01f1ef8b70dc34813f134cc23ee447c27613d8d4bafc59b601f6b92cd3118f81&r=dXMtZWFzdDE%3D"></video>
+            <video   className='h-full w-full object-cover' autoPlay loop muted src="/src/assets/heroVideo1.mp4"></video>
         </div>
   )
 }
