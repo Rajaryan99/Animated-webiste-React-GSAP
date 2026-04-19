0b3707478554dc335fba6e574bec52342484c4f8
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-19 21:48:31 +0530

    added a hover efferct in navigation

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 export default function HomeBottomText() {
   return (
     <div className='text-white mb-4 flex items-center justify-center gap-4'>
-      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0 uppercase rounded-full font-medium' to='/projects'>Work</Link>
-      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0  uppercase rounded-full font-medium' to='/agents'>agency</Link>
+      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0 hover:border-[#d3fd50] hover:text-[#d3fd50] uppercase rounded-full font-medium'   to='/projects'>Work</Link>
+      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0 hover:border-[#d3fd50] hover:text-[#d3fd50] uppercase rounded-full font-medium'  to='/agents'>agency</Link>
     </div>
   )
 }2
