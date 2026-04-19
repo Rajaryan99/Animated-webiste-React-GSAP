5329058375037629de18bbb79e449e025e299b9f
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-20 00:11:57 +0530

    team image fix to its relative path

--- src/pages/Agents.jsx
+++ src/pages/Agents.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 export default function Agents() {
   return (
     <div className='bg-white'>
-      <div className='text-center mt-[58vh]'> 
-        <h1 className='text-[20vw] font-[font2] leading-[17vw]'>SEVEN7Y TWO</h1>
+      <div className=' absolute  h-80 w-55 rounded-3xl bg-red-700 overflow-hidden object-cover  left-115 top-55'>
+          <img src="/src/assets/team/CAMILLE_640X960_2.jpg" className=''  alt=""  />
+      </div>
+      <div className=' relative text-center mt-[58vh]'> 
+        <h1 className='text-[20vw] z-2 font-[font2] leading-[17vw]'>SEVEN7Y TWO</h1>
       </div>
       <div className='pl-[40%] pr-[5%]'>
         <p className='font-[font2] text-6xl font-bold '>&emsp;&emsp;&emsp;&emsp;&emsp;We're inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
@@ -12,3 +15,4 @@ export default function Agents() {
     </div>
   )
 }
+  
