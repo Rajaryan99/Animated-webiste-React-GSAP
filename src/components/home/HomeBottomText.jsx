a50ecc6f63dbc454c0032b90629ce8038652547a
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-19 21:42:14 +0530

    added a home page with navigation butons

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function HomeBottomText() {
   return (
-    <div>HomeBottomText</div>
+    <div className='text-white mb-4 flex items-center justify-center gap-4'>
+      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0 uppercase rounded-full font-medium' to='/projects'>Work</Link>
+      <Link  className=' border-2 text-[6.5vw] leading-[6vw] px-8 py-1 pt-0 pb-0  uppercase rounded-full font-medium' to='/agents'>agency</Link>
+    </div>
   )
-}
+}2
