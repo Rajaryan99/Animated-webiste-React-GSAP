fc7ed93af8da97f60c2f6f0ab56443ca7d6d72de
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-19 23:31:45 +0530

    agency page home page added

--- src/pages/Agents.jsx
+++ src/pages/Agents.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Agents() {
   return (
-    <div>Agents</div>
+    <div className='bg-white'>
+      <div className='text-center mt-[58vh]'> 
+        <h1 className='text-[20vw] font-[font2] leading-[17vw]'>SEVEN7Y TWO</h1>
+      </div>
+      <div className='pl-[40%] pr-[5%]'>
+        <p className='font-[font2] text-6xl font-bold '>&emsp;&emsp;&emsp;&emsp;&emsp;We're inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
+    </div>
   )
 }
