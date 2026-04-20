9450b78682a54b891aa6e2e43c3f13ec62f27ebf
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-21 01:36:54 +0530

    added a stair animation to the webite

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,43 @@ import './App.css'
 import Home from './pages/Home'
 import Agents from './pages/Agents'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 function App() {
 
 
+  useGSAP(() => {
+
+    const tl = gsap.timeline()
+
+    tl.from('.stair', {
+      height:0,
+      duration:1,
+      stagger:{
+        amount:-0.3
+      }
+
+    })
+    tl.to('.stair', {
+      y:'100%',
+      stagger:{
+        amount:-0.3
+      }
+    })
+  })
+
+
   return (
     <>
+
+    <div className='h-screen w-full fixed z-10  flex'>
+      <div className=' stair h-full w-1/5 bg-black'> </div>
+      <div className=' stair h-full w-1/5 bg-black'> </div>
+      <div className=' stair h-full w-1/5 bg-black'> </div>
+      <div className=' stair h-full w-1/5 bg-black'> </div>
+      <div className=' stair h-full w-1/5 bg-black'> </div>
+    </div>
 
   <Routes>
     <Route path='/' element={<Home/>} />
