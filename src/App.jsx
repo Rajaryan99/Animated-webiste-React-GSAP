984f3332b793f74607c401aae21e7134f22dbcef
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-28 00:32:41 +0530

    added a stair animation effect setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import './App.css'
 import Home from './pages/Home'
 import Agents from './pages/Agents'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 
 function App() {
 
+useGSAP(() => {
+  gsap.from('.stair', {
+      height:0,
+      stagger:{
+        amount:-0.3
+      }
 
+  })
+})
   return (
     <>
+    <div className='h-screen w-full flex fixed z-20 top-0' >
+      <div className='stair h-full w-1/5 bg-black'></div>
+      <div className='stair h-full w-1/5 bg-black'></div>
+      <div className='stair h-full w-1/5 bg-black'></div>
+      <div className='stair h-full w-1/5 bg-black'></div>
+      <div className='stair h-full w-1/5 bg-black'></div>
+    </div>
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/agents' element={<Agents/>} />
