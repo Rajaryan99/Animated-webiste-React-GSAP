1fa4b802c3453d3a96857d4febbef55137c5dcca
Raj Aryan <raj.aryan2518@gmail.com>
2026-04-30 00:56:30 +0530

    added a display none property

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,56 @@ import Agents from './pages/Agents'
 import Projects from './pages/Projects'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { useRef } from 'react'
 
 
 function App() {
 
+  const stairParentDivRef = useRef(null);
+
+  console.log(stairParentDivRef)
+
 useGSAP(() => {
-  gsap.from('.stair', {
+
+  const tl = gsap.timeline();
+
+  tl.to(stairParentDivRef.current,{
+    display:"block"
+  })
+
+  tl.from('.stair', {
       height:0,
       stagger:{
-        amount:-0.3
+        amount:-0.25
       }
 
   })
+  tl.to('.stair', {
+    y:"100%",
+     stagger:{
+        amount:-0.25
+      }
+
+  })
+
+  tl.to(stairParentDivRef.current,{
+    display:"none"
+  })
+   tl.to('.stair', {
+    y:"0%",
+   })
+     
 })
   return (
     <>
-    <div className='h-screen w-full flex fixed z-20 top-0' >
+    <div ref={stairParentDivRef} className='h-screen w-full  fixed z-20 top-0'>
+    <div className='h-screen w-full flex ' >
       <div className='stair h-full w-1/5 bg-black'></div>
       <div className='stair h-full w-1/5 bg-black'></div>
       <div className='stair h-full w-1/5 bg-black'></div>
       <div className='stair h-full w-1/5 bg-black'></div>
       <div className='stair h-full w-1/5 bg-black'></div>
+    </div>
     </div>
   <Routes>
     <Route path='/' element={<Home/>} />
