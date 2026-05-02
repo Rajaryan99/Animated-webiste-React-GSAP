72ba1053dce49f35dc23c6e39c86bf7daea16813
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-02 23:42:51 +0530

    animating the website accoring to render

--- src/components/common/Stairs.jsx
+++ src/components/common/Stairs.jsx
@@ -3,12 +3,15 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { useRef } from 'react'
 import { useLocation } from 'react-router-dom';
+import '../../App.css';
 
-export default function Stairs() {
+export default function Stairs(props) {
 
    const stairParentDivRef = useRef(null);
 
    const currentPath = useLocation().pathname;
+   const pageRef = useRef(null)
+
 
 
 useGSAP(() => {
@@ -40,12 +43,25 @@ useGSAP(() => {
    tl.to('.stair', {
     y:"0%",
    })
+
+   gsap.from(pageRef.current,{
+      opacity:0,
+      delay:1.2
+   })
+   
      
 }, [currentPath])
 
 
+
+console.log(props.children)
+
+
+
   return (
-       <div ref={stairParentDivRef} className='h-screen w-full  fixed z-20 top-0'>
+      <div>
+        
+         <div ref={stairParentDivRef} className='h-screen w-full  fixed z-20 top-0 '>
     <div className='h-screen w-full flex ' >
       <div className='stair h-full w-1/5 bg-black'></div>
       <div className='stair h-full w-1/5 bg-black'></div>
@@ -54,5 +70,9 @@ useGSAP(() => {
       <div className='stair h-full w-1/5 bg-black'></div>
     </div>
     </div>
+    <div ref={pageRef} className='pageDiv bg-black'>
+    {props.children}
+    </div>
+      </div>
   )
 }
